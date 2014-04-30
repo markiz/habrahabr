5ab700b9c7424a78971d7825c0f43246cfc450c2
Mark Abramov <markizko@gmail.com>
2014-04-30 15:06:23 +0400

    Handle inconsistencies of next_page / next_link response attributes

--- lib/habrahabr/entities/list_result.rb
+++ lib/habrahabr/entities/list_result.rb
@@ -1,11 +1,14 @@
 module Habrahabr
   module Entities
     class ListResult
+      class NextLink < Base
+      end
+
       attr_reader :pages_count, :next_link, :data
       def initialize(attrs, data_coercion_class = nil)
         attrs = HashWithIndifferentAccess.new(attrs)
         @pages_count = attrs.fetch(:pages) { 1 }
-        @next_link = attrs.fetch(:next_link) { nil }
+        @next_link = NextLink.new(attrs[:next_link] || attrs[:next_page] || {})
         @data = attrs.fetch(:data)
         @data = @data.map {|item| data_coercion_class.new(item) } if data_coercion_class
       end
